725fea072a18d911f9ecd8e3659c40cdec861dac
Joey Chen <jay.hatelove@gmail.com>
2018-11-05 16:09:38 +0800

    refactor test

--- src/AccountingTests.java
+++ src/AccountingTests.java
@@ -5,11 +5,18 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountingTests {
+
+	private final StubBudgetRepository budgetRepository = new StubBudgetRepository();
+	private final Accouting accouting = new Accouting(budgetRepository);
+
 	@Test
 	public void no_budgets() {
 
-		Accouting accouting = new Accouting(new StubBudgetRepository());
-		Double totalAmount = accouting.TotalAmount(LocalDate.of(2010, 4, 1), LocalDate.of(2010, 4, 1));
-		assertEquals(0, totalAmount, 0.01);
+		totalAmountShouldBe(0, LocalDate.of(2010, 4, 1), LocalDate.of(2010, 4, 1));
+	}
+
+	private void totalAmountShouldBe(int expected, LocalDate start, LocalDate end) {
+		Double totalAmount = accouting.TotalAmount(start, end);
+		assertEquals(expected, totalAmount, 0.01);
 	}
 }
